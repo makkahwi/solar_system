ddfc9ea98adecf76ade6cfd34bd0b92a327de9a2
makkahwi <suhaibahmadai@hotmail.com>
2023-03-31 17:37:53 +0300

    Engage Missed Path File

--- src/ThreeDModel/Planet/index.jsx
+++ src/ThreeDModel/Planet/index.jsx
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import PlanetPath from "./Path";
 import RotatingPlanet from "./RotatingPlanet";
 
 export default function Planet({ ...props }) {
@@ -6,13 +7,7 @@ export default function Planet({ ...props }) {
     <Fragment>
       <RotatingPlanet {...props} />
 
-      <mesh visible rotation={[0, 0, 0]} castShadow position={[0, 0, 0]}>
-        <ringBufferGeometry
-          args={[props.position - props.size / 2, props.position, 100]}
-        />
-
-        <meshPhysicalMaterial color={props.color} />
-      </mesh>
+      <PlanetPath {...props} />
     </Fragment>
   );
 }
