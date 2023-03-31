20fa1cc60bdefd4bcf910cda84d1ad52dece7a41
makkahwi <suhaibahmadai@hotmail.com>
2023-03-31 14:23:47 +0300

    Add Circular Motion Infra

--- src/ThreeDModel/Planets/RotatingPlanet.jsx
+++ src/ThreeDModel/Planets/RotatingPlanet.jsx
@@ -1,18 +1,33 @@
 import { useFrame } from "@react-three/fiber";
+import { useState } from "react";
 import { useRef } from "react";
 import { Fragment } from "react";
 
-export default function RotatingPlanet({ args, color, planetName, ...props }) {
+export default function RotatingPlanet({
+  args,
+  color,
+  planetName,
+  position: initialPosition,
+  ...props
+}) {
   function Rotating() {
+    const [position, setPosition] = useState(initialPosition);
     const myMesh = useRef();
 
     useFrame(({ clock }) => {
       const a = clock.getElapsedTime();
       myMesh.current.rotation.z = a;
+      setPosition([initialPosition[0], initialPosition[1], initialPosition[2]]);
     });
 
     return (
-      <mesh {...props} receiveShadow castShadow ref={myMesh}>
+      <mesh
+        {...props}
+        receiveShadow
+        castShadow
+        ref={myMesh}
+        position={initialPosition}
+      >
         <sphereBufferGeometry args={args} />
 
         <meshPhysicalMaterial color={color} />
