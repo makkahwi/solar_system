273ebb9615bb01545391d897a97a39e3a8b224a0
makkahwi <suhaibahmadai@hotmail.com>
2023-04-10 15:42:48 +0300

    Add Sun Rotation

--- src/ThreeDModel/Sun.jsx
+++ src/ThreeDModel/Sun.jsx
@@ -1,13 +1,23 @@
 import { planetsScale } from "@/utils/consts";
-import { useLoader } from "@react-three/fiber";
+import { useFrame, useLoader } from "@react-three/fiber";
+import { useRef } from "react";
 import { TextureLoader } from "three";
 
 export default function Sun(props) {
   const texture = useLoader(TextureLoader, "/sun.jpg");
+  const myMesh = useRef();
 
   const sunSize = 1392700; // acutal diameter by KM
+
+  useFrame(({ clock }) => {
+    const a = clock.getElapsedTime();
+    // myMesh.current.rotation.x = a;
+    // myMesh.current.rotation.y = a;
+    myMesh.current.rotation.z = -a;
+  });
+
   return (
-    <mesh {...props}>
+    <mesh {...props} ref={myMesh}>
       <pointLight castShadow />
       <sphereBufferGeometry args={[sunSize * planetsScale, 30, 10]} />
       <meshPhongMaterial emissive="#d24d00" map={texture} />
